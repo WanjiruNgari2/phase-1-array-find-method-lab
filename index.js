4b9ca33e3e092b61c6646c542eb93497640e4d15
Damaris Ngari <wanjirungari2@gmail.com>
2024-12-08 04:50:14 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+//function superbowlWin()
+  function superbowlWin (records) {
+    const record = [
+        { year: "2015", result: "W"},
+        { year: "2014", result: "N/A"},
+        { year: "2013", result: "L"},
+      ]
+      for (let i = 0; i < records.length; i++) {
+        if(records[i].result === "W") {
+            return records[i].year;
+          }
+            else {
+            console.log("undefined");
+            }
+      }
+      
+      }
+  
